8332c272de19aa5192d52b512eb08523e122e898
frostyden <fdenfrost@gmail.com>
2019-06-03 22:40:57 +0400

    local psql connection, serve static file in route

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require("path");
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const upload = multer();
 const port = process.env.PORT || 8080;
 
-//STATIC ROUTES
-app.use(express.static('public_files'));
+//DB
+const pgp = require('pg-promise')(/* options */);
+const db = pgp('postgres://todouser:abc123!@localhost:5432/tododb');
+
 
 //ROUTES
 const loginRoute = require('./routes/login.js');
@@ -14,12 +20,35 @@ const moshiRoute = require('./routes/moshi.js');
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+//PARSING
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(upload.array());
+
+//STATIC ROUTES
+//app.use(express.static('public_files'));
+//app.use('/file', express.static('public_files'));
+app.use('/file', express.static(path.join(__dirname, 'public_files')));
+
 //ROUTE USERS
 app.use('/login', loginRoute);
 app.use('/homepage', homepageRoute);
 app.use('/moshi', moshiRoute);
 
-//app.use('/moshi', express.static('public_files'));
-//app.use(express.static('public_files'));
+app.get('/test', function (req, res) {
+	try {
+		const data = db.any('SELECT * FROM playground WHERE equip_id = $1', 1);
+		// success
+		console.log(data+' SOME DATA');
+		res.send(data);
+	}
+	catch (e) {
+		// error
+		console.log(e);
+	}
+	//db.$pool.end();
+	//pgp.end();
+	//res.send(data);
+ });
 
 app.listen(port, () => console.log(`Listening in port ${port}`));
--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,15 @@
- 
- const express = require('express');
- const router = express.Router();
 
- // All routes begin with /login/...
- router.get('/', function(req, res){
-    res.render('login');
- });
+const express = require('express');
+const router = express.Router();
 
- module.exports = router;
+// All routes begin with /login/...
+router.get('/', function (req, res) {
+   res.render('login');
+});
+
+router.post('/', function (req, res) {
+   console.log(req.body);
+   res.send(req.body);
+});
+
+module.exports = router;
