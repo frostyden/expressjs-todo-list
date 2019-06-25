5c1b7ea1148588d3d9f9771f70a438216c185c31
frostyden <fdenfrost@gmail.com>
2019-06-25 21:05:36 +0400

    test separate routes with updated db tables

---
+++ routes.js
@@ -0,0 +1,8 @@
+//ROUTES
+const loginRoute = require('./routes/login.js');
+const homepageRoute = require('./routes/homepage.js');
+const moshiRoute = require('./routes/moshi');
+
+/*module.exports = {
+	
+};*/
