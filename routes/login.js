0c65f6ac4ae2e14b8f82bbb26ba61b0a58f0ac72
frostyden <fdenfrost@gmail.com>
2019-07-06 00:52:44 +0400

    Change behavior, return empty array when query does not match

--- routes/login.js
+++ routes/login.js
@@ -1,31 +1,39 @@
 
 const express = require('express');
 const router = express.Router();
-const dbRequire = require('../config/db.js');
+const pg = require('../config/db.js');
 
 // All routes begin with /login/...
 router.get('/', function (req, res) {
 	res.render('login');
 });
 
-router.post('/', function (req, res) {
+router.post('/', (req, res) => {
 	if (!req.body.username || !req.body.password) {
-		res.status(400).send('Current username and password pair is incorrect.');
+		res.status(400).send('Invalid username/password pair.'); // Bad request. Empty field(s)
 	} else {
-		dbRequire.db.any('SELECT u_username, password FROM tdlist_user where u_username=$1 and password =$2', [req.body.username, req.body.password])
-			.then(function (data) {
-				// success;
-				if ()
-				console.log('DATA:', data);
-				res.status(200).send(data);
+		pg.select('u_id', 'u_username', 'password')
+			.from('tdlist_user')
+			.where('u_username', '=', pg.raw('?', [req.body.username]))
+			.andWhere('password', '=', pg.raw('?', [req.body.password]))
+			.unionAll(pg.raw('SELECT NULL, NULL, NULL'))
+			.then(data => {
+				if (data[0].u_username !== req.body.username) {
+					console.log('DATA:', data);
+					res.status(204).send('There is no such user.'); // No Content. The server successfully processed the request and is not returning any content.
+				} else if (data[0].password !== req.body.password) {
+					console.log('DATA:', data[0].password);
+					res.status(400).send('Password is wrong.'); // Bad request for now
+				} else {
+					res.status(200).send('Logged in.'); // Success
+				}
 			})
-			.catch(function (error) {
-				// error;
-				console.log('ERROR:', error);
+			.catch(error => {
+				console.error('ERROR: ', error);
+				res.status(400).send('Fail.');
 			});
 	}
-	// console.log(req.body);
-	// res.send(req.body);
 });
+
 
 module.exports = router;
