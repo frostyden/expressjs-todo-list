278af76096fe5dfddd0318700db60e04f98acd84
frostyden <fdenfrost@gmail.com>
2019-08-02 23:16:32 +0400

    Add get request to the route

--- routes/homepage.js
+++ routes/homepage.js
@@ -1,12 +1,30 @@
 
 const express = require('express');
 const router = express.Router();
+const pg = require('../config/db.js');
 
-router.get('/', function (req, res) {
-	res.render('homepage');
-});
-router.get('/:userid', function (req, res) {
-	res.render('homepage');
+// URL /api/homepage/...
+
+// router.get('/', (req, res) => {
+// 	res.send('homepage');
+// });
+
+router.get('/:id', (req, res) => {
+	pg.select('status', 'description')
+		.from('tl_task')
+		.where('ownid', '=', pg.raw('?', req.params.id))
+		.then(data => {
+			res.render('homepage', {
+				// username: req.params.username, 
+				id: req.params.id,
+				status: data[0].status, 
+				description: data[0].description
+			});
+		})
+		.catch(error => {
+			console.error('ERROR: ', error);
+			res.status(400).send('Fail.');
+		});
 });
 
 module.exports = router;
